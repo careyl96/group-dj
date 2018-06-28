2467d243188505f12aa2cc8c33dc17d5fa247d06
Carey Lee <careyl96@gmail.com>
2018-06-28 13:33:51 -0700

    modified original structure

---
+++ server/factories.js
@@ -0,0 +1,14 @@
+const createUser = (nickname) => {
+};
+
+const addUser = (user, userList) => {
+};
+
+const removeUser = (userID, userList) => {
+};
+
+module.exports = {
+  createUser,
+  addUser,
+  removeUser,
+};
--- server/socket-manager.js
+++ server/socket-manager.js
@@ -1,18 +1,10 @@
-const { VERIFY_USER, USER_CONNECTED, LOGOUT } = require('../client/src/events');
+const io = require('./index');
+const { ADD_USER, USER_CONNECTED, LOGOUT } = require('../client/src/events');
 
-const connectedUsers = {};
+let connectedUsers = {};
 
 const handleSocketEvents = (socket) => {
   console.log(`Socket ID: ${socket.id}`);
-  socket.on(VERIFY_USER, () => {
-    console.log('verifying user');
-  });
-  socket.on(USER_CONNECTED, () => {
-    console.log('user connected');
-  });
-  socket.on(LOGOUT, () => {
-    console.log('user logged out');
-  });
 };
 
 module.exports = handleSocketEvents;
